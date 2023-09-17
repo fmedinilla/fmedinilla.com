f9ade2a642878fd69a53d18ebef470da757925ea
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2023-09-17 18:55:25 +0200

    update web

---
+++ src/data/projects.js
@@ -0,0 +1,13 @@
+/*
+    PROJECT
+        id: string
+        name: string
+        img: string (link to image)
+        techs: string[]
+        page: string (link to page)
+        primary: bool
+*/
+
+export const projects = [];
+export const numProjects = projects.length;
+export const noProjects = projects.length === 0;
